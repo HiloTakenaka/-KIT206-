1c91d0700e9b857427a5d2f32cabb8065e9567f3
geoshad <64393689+geoshad@users.noreply.github.com>
2022-04-11 15:38:02 +1000

    Add files via upload

---
+++ Unit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace assign2
+{
+    public class Unit
+    {
+        public string UnitCode { get; set; } // e.g. KIT206
+        public string UnitTitle { get; set; } // e.g. Software Design & Development
+        public Staff UnitCoordinator { get; set; } // Staff Class / foreign key
+
+        // constructor for returning full unit string
+        public override string ToString()
+        { 
+            return $"{Code} {Title}";
+        }
+    }
+}
