772f90854a3f184633b6de8954aab0f46802f5fb
georgie <64393689+geoshad@users.noreply.github.com>
2022-04-11 23:12:56 +1000

    Add files via upload

---
+++ Consultation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace assign2
+{
+    class Consultation
+    {
+        public Staff Staff { get; set; } // Staff Class / foreign key
+        public Day Day { get; set; } // Day enum (in UnitClass Class)
+        public int StartTime { get; set; }
+        public int EndTime { get; set; }
+    }
+}
