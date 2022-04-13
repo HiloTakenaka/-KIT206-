2ab533b54481776265e16fb9c81890c2dbda40c9
HiloTakenaka <84254202+HiloTakenaka@users.noreply.github.com>
2022-04-13 17:42:23 +1000

    Add files via upload

--- Consultation.cs
+++ Consultation.cs
@@ -1,15 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
 
-namespace assign2
-{
-    class Consultation
+    namespace assign2
     {
-        public Staff Staff { get; set; } // Staff Class / foreign key
-        public Day Day { get; set; } // Day enum (in UnitClass Class)
-        public int StartTime { get; set; }
-        public int EndTime { get; set; }
+        //enum for selecting title
+        public int staffID;
+        public enum Day { Mon, Tues, Wed, Thurs, Fri }
+        public int startHour;
+        public int endHour;
+        //enum for selecting campus
+    
+        // enum for staff category
+
+        public class Consultation // Consultation Details for 
+        {
+            public int staffID{ get; set; }
+            public enum Day { get; set; }
+            public int startHour{ get; set; }
+            public int endHour{ get; set; }
+
+
+            // methods for turning enum values into strings
+
+            public string DayString()
+            {
+                switch (Day)
+                {
+                case Title.Mon:
+                    return "Monday";
+                case Title.Tues:
+                    return "Tuesday";
+                case Title.Wed:
+                    return "Wednesday";
+                case Title.Thurs:
+                    return "Thursday";
+                case Title.Fri:
+                    return "Friday";
+                default:
+                    return "";
+                }
+            }
+
+        
+        }
     }
-}
